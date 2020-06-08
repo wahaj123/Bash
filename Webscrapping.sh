7f364292b2b7ee41cd5e4fd9c15c6395803fa460
wahaj123 <wahajakm1234@gmail.com>
2020-06-08 11:50:55 -0700

    Files added

--- Webscrapping.sh
+++ Webscrapping.sh
@@ -4,26 +4,40 @@ grep 'href=\"' | \
 sed 's/.*href="//g;s/".*//g' > URL.txt
 sed -i 's/amp;//' URL.txt
 
+curl -sL https://www.mcdelivery.com.pk/pk/browse/menu.html | grep -o '<li class="secondary-menu-item ">.*</li>' | sed 's/<[^>]\+>//g' > deals.txt
+
 ARRAY=()
 while read -r LINE
 do
     ARRAY+=("$LINE")
 done < URL.txt
 
+deals=()
+while read -r main
+do
+    deals+=("$main")
+done < deals.txt
+i= 0
+
 for LINE in "${ARRAY[@]}"
 do    
     echo $LINE
-    var = $(curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep '<span>.*</span>' | sed 's/<[^>]\+>//g')
-    curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" > $var.txt
-    #  var ="$(curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep '<span>.*</span>' | sed 's/<[^>]\+>//g')" >> 1234.txt
-    #  echo $var
-     curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep -o '<h5 class="product-title">.*</h5>' | sed 's/<[^>]\+>//g' >> name.txt
-     curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep -o '<span class="starting-price">.*</span>' | sed 's/<[^>]\+>//g' >> price.txt 
+    echo ${deals[$i]}
+    i=$(( $i + 1));
+    touch ${deals[$i]}.txt
+    curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep -o '<h5 class="product-title">.*</h5>' | sed 's/<[^>]\+>//g' >> name.txt
+    curl https://www.mcdelivery.com.pk/pk/browse/menu.html"$LINE" | grep -o '<span class="starting-price">.*</span>' | sed 's/<[^>]\+>//g' >> price.txt 
+    #   for LINE in $(nl name.txt |awk '{ print $1 }'); do
+    #   echo "Name: ($(sed -n "${LINE}p" name.txt) \n Price $(sed -n "${LINE}p" price.txt))"
+    #   echo -en '\n'
+    #   done 
+    for LINE in $(nl name.txt |awk '{ print $1 }'); do
+    echo -e "$LINE- name: $(sed -n "${LINE}p" name.txt)\n   Price: $(sed -n "${LINE}p" price.txt)\n"
+    done >> "${deals[$i]}".txt
 done
-for LINE in $(nl name.txt |awk '{ print $1 }'); do
-  echo "($(sed -n "${LINE}p" name.txt), $(sed -n "${LINE}p" price.txt))"
-  echo -en '\n'
-done >> deals.txt  
+
+  
+
 #paste -d , name.txt price.txt > price+name.txt
 #class="secondary-menu-item-target"
 
